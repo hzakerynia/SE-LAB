7aaa96a99b96486ff4bc9e9cf5064d0960dbb988
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-11-09 18:54:51 +0330

    remove idempotent operation

--- MiniJava/src/main/java/parser/ParseTable.java
+++ MiniJava/src/main/java/parser/ParseTable.java
@@ -23,7 +23,6 @@ public class ParseTable {
                 try {
                     nonTerminals.put(i, NonTerminal.valueOf(temp));
                 }catch (Exception e){
-                    temp = temp;
                 }
             }
             else {
